9bc3e7f6f09092f01cca93968dacf8b0d090b7cc
felipe-alvarez <113711798+felipe-alvarez@users.noreply.github.com>
2024-04-21 15:13:53 +0100

    - EX commented and done

--- EX6/binary_symetric_channel.py
+++ EX6/binary_symetric_channel.py
@@ -1,13 +1,15 @@
 import random
 
 
+# EX 6, a
+# Function to simulate a binary symmetric channel that receives a binary sequence as string and a probability p of BER
 def binary_symmetric_channel(binary_sequence, p):
     result = ""
     for bit in binary_sequence:
+        # For each bit in the binary sequence, generate a random number between 0 and 1 and compare it with the BER
         if random.random() < p:
             result += '0' if bit == '1' else '1'
+        # If the random number is greater than the BER, the bit is transmitted without errors
         else:
             result += bit
     return result
-
-
--- EX6/simulate_bsc_sequence.py
+++ EX6/simulate_bsc_sequence.py
@@ -2,22 +2,27 @@ import random
 
 from EX6.binary_symetric_channel import binary_symmetric_channel
 
-sequence_lengths = [1024, 10240, 102400, 1024000, 10240000]
-probability = 0.1
+sequence_lengths = [1024, 10240, 102400, 1024000, 10240000]  # Sequence lengths
+probability = 0.1  # BER probability
 
 
+# EX 6, b
+# Function to simulate a binary symmetric channel that receives the length of the sequence and a probability p of BER
 def simulate_bsc_sequence(L, p):
+    # Generate a random binary sequence of length L as string
     in_sequence = ''.join(random.choice('01') for _ in range(L))
+    # Transmit the sequence through the binary symmetric channel
     out_sequence = binary_symmetric_channel(in_sequence, p)
+    # Calculate the number of errors
     num_errors = sum(a != b for a, b in zip(in_sequence, out_sequence))
-
+    # Calculate the real BER
     real_ber = num_errors / L
-
     return real_ber
 
 
 def main():
     for L in sequence_lengths:
+        # Calculate the BER for each sequence length
         ber = simulate_bsc_sequence(L, probability)
         print(f"BER for L = {L} is {ber} with p = {probability}")
 
